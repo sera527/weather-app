fe10129ef7dc1a08701290008870563c57a83774
Serhii Posternak <serhiiposternak@doubledmarketing.com>
2025-05-21 02:39:15 +0300

    add migration

---
+++ database/migrations/2025_05_19_000000_create_weather_subscriptions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Enums\FrequencyType;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('weather_subscriptions', function (Blueprint $table) {
+            $table->id();
+            $table->string('email')->unique(); // Робимо email унікальним
+            $table->string('city');
+            $table->enum('frequency', FrequencyType::values());
+            $table->boolean('is_active')->default(false);
+            $table->string('token', 64)->unique();
+            $table->timestamp('last_sent_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('weather_subscriptions');
+    }
+};
