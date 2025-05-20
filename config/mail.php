9b95cc4f7c5cb9f2865753622009bd6eeb6edd03
Serhii Posternak <serhiiposternak@doubledmarketing.com>
2025-05-21 02:38:57 +0300

    use resend

--- config/mail.php
+++ config/mail.php
@@ -58,6 +58,10 @@ return [
             // ],
         ],
 
+        'resend' => [
+            'transport' => 'resend',
+        ],
+
         'mailgun' => [
             'transport' => 'mailgun',
             // 'client' => [
