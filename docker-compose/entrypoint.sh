d109edf581cefdcc34d2f732cfcd5e7988511011
Serhii Posternak <serhiiposternak@doubledmarketing.com>
2025-05-21 04:18:54 +0300

    fix entrypoint.sh

--- docker-compose/entrypoint.sh
+++ docker-compose/entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 set -e
 
+if [ ! -d "vendor" ]; then
+    echo "Vendor directory not found. Running composer install..."
+    composer install --no-progress --no-interaction
+else
+    echo "Vendor directory found. Skipping composer install."
+fi
+
 if [ ! -f ".env" ]; then
     echo "Creating .env file from .env.example"
     cp .env.example .env
@@ -10,13 +17,6 @@ fi
 if grep -q '^APP_KEY=$' .env || ! grep -q '^APP_KEY=' .env ; then
     echo "APP_KEY is empty or not set, generating new one..."
     php artisan key:generate --ansi --force
-fi
-
-if [ ! -d "vendor" ]; then
-    echo "Vendor directory not found. Running composer install..."
-    composer install --no-progress --no-interaction
-else
-    echo "Vendor directory found. Skipping composer install."
 fi
 
 echo "Running database migrations..."
